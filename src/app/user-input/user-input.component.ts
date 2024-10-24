e1df5a9ac9e802ced2c2d1e11de2d469901669e0
Richard Burman <richard.burman@harperjames.co.uk>
2024-10-24 14:13:14 +0100

    Refactor user input component to include form submission and improve input fields

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -9,7 +9,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  
   onSubmit() {
     console.log('Form submitted!');
+    console.log('Initial Investment: ' + this.enteredInitialInvestment);
+    console.log('Annual Investment: ' + this.enteredAnnualInvestment);
+    console.log('Expected Return: ' + this.enteredExpectedReturn);
+    console.log('Duration: ' + this.enteredDuration);
   }
 }
